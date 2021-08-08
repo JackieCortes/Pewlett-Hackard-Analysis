aa6ba79aedefbd45bb476f122da68e9bad2595ee
Maria Jackeline Cortes <86242690+JackieCortes@users.noreply.github.com>
2021-08-07 20:39:18 -0500

    Add files via upload

---
+++ Extra/Extra.sql
@@ -0,0 +1,27 @@
+SELECT COUNT(title),title
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_elig2
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '01/01/9999')
+AND (e.birth_date BETWEEN '1956-01-01' AND '1964-12-31')
+ORDER BY emp_no;
+
+SELECT COUNT(title),title
+INTO out_ofrange
+FROM mentorship_elig2
+GROUP BY title
+ORDER BY count DESC;
